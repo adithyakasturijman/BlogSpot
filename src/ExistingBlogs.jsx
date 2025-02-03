ee49dc834679a8071310cb8a7899a74ac654a280
Adithya Kasturi <adithya.k@jmangroup.com>
2025-02-03 18:18:00 +0530

    My first Commit

---
+++ src/ExistingBlogs.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ExistingBlogs = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ExistingBlogs
---
+++ src/NavBar.jsx
@@ -0,0 +1,42 @@
+const NavBar =()=>{
+    return (
+        <nav className="bg-black w-full fixed top-0 left-0">
+      <div className="flex items-center justify-between h-16 px-6 sm:px-12">
+        <div className="flex items-center ">
+          <img src="/blogger-logo-free-png.webp" className="w-15 h-15" alt="Logo" />
+          <a href="/" className="text-xl font-semibold text-white hover:text-gray-300">
+            BlogSpot
+          </a>
+          <div className="flex items-center ml-30">
+          <ul className="hidden sm:flex items-center space-x-15">
+          <li className="flex items-center space-x-2">
+            <img src="/home_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png" className="w-5 h-5" alt="Home" />
+            <a href="/" className="text-md font-medium text-white hover:text-gray-300">Home</a>
+          </li>
+          <li className="flex items-center space-x-2">
+            <img src="/groups_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png" className="w-5 h-5" alt="About" />
+            <a href="/about" className="text-md font-medium text-white hover:text-gray-300">About</a>
+          </li>
+          <li className="flex items-center space-x-2">
+            <img src="/contact_page_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png" className="w-5 h-5" alt="Contact" />
+            <a href="/contact" className="text-md font-medium text-white hover:text-gray-300">Contact</a>
+          </li>
+        </ul>
+          </div>
+        </div>
+
+        
+
+        <div className="hidden sm:flex space-x-4">
+          <a href="/login" className="bg-[#36454F] text-white px-4 py-1 rounded-full hover:bg-gray-600">Login</a>
+          <a href="/signup" className="bg-[#36454F] text-white px-4 py-1 rounded-full hover:bg-gray-600">Signup</a>
+        </div>
+
+        <div className="sm:hidden">
+          <img src="/menu_24dp_FFFFFF_FILL0_wght400_GRAD0_opsz24.png" className="w-6 h-6" alt="Menu" />
+        </div>
+      </div>
+    </nav>
+    )
+}
+export default NavBar;
